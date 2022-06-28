514530787bfadc9e966a7a10209ed894fb2e7f5b
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-06-28 13:47:20 +0900

    Controller 설정을 위한 내용 - route.js에서 API에 따라 호출되는 메소드들을 구현합니다.

---
+++ app/mysql/controller/controller.js
@@ -0,0 +1,118 @@
+const db = require('../model/index.js');
+const Sensor = db.sensor;
+const Op = db.sequelize.Op;
+
+// Create tutorial
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.value) {
+        res.status(400).send({
+            message: 'Value is empty!'
+        });
+        return;
+    }
+
+    // Set tutorial
+    const sensor = {
+        value: req.body.value,
+        create_at: req.body.create_at
+    };
+
+    // Save tutorial
+    Sensor
+        .create(sensor)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Create sensor failure.'
+            });
+        });
+};
+
+// Retrieve all tutorials
+exports.findAll = (req, res) => {
+    const value = req.query.value;
+    let condition = { where: {} };
+
+    Sensor
+        .findAll(condition)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Retrieve all sensor failure.'
+            });
+        });
+};
+
+// Retrieve tutorial by id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Sensor
+        .findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Retrieve sensor failure. (id: ' + id + ')'
+            });
+        });
+};
+
+// Update tutorial by id
+exports.update = (req, res) => {
+    const id = req.params.id;
+    const condition = id ? { where: { id: id } } : null;
+
+    Sensor
+        .update(
+            req.body,
+            condition
+        )
+        .then(resultCount => {
+            if (resultCount == 1) {
+                res.send({
+                    message: 'Sensor updated.'
+                });
+            } else {
+                res.send({
+                    message: 'Cannot update sensor. (id: ' + id + ')'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Update sensor failure. (id: ' + id + ')'
+            });
+        });
+};
+
+// Delete tutorial by id
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    const condition = id ? { where: { id: id } } : null;
+
+    Sensor
+        .destroy(condition)
+        .then(resultCount => {
+            if (resultCount == 1) {
+                res.send({
+                    message: 'Tutorial deleted.'
+                });
+            } else {
+                res.send({
+                    message: 'Cannot delete tutorial. (id: ' + id + ')'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Delete tutorial failure. (id: ' + id + ')'
+            });
+        });
+};
