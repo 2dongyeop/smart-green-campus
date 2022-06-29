7c98f7fd9c338a313eaa05916b24b5f998d618c6
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-06-29 15:29:09 +0900

    api-server 구현 코드

---
+++ api-server/app/mysql/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    host: 'localhost',
+    username: 'root',
+    password: 'dlehdduq1!',
+    db: 'greenDB',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
---
+++ api-server/app/mysql/model/model.js
@@ -0,0 +1,19 @@
+module.exports = (sequelizeConfig, Sequelize) => {
+    // Set Model
+    const Sensor = sequelizeConfig.define(
+        'sensor',
+        {
+            sensor_name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            value: {
+                type: Sequelize.DOUBLE,
+                allowNull: false,
+                // unique: true,
+            }
+        }
+    );
+
+    return Sensor;
+};
