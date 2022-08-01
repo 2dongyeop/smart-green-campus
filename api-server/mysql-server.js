a9251559d35d672f5360d0cd651b69733d4c29a9
leedongyeop <ldy_1204@naver.com>
2022-08-01 13:02:07 +0900

    firebase-fundamental 레포를 따로 생성하여, 학습 후 적용합니다.

--- api-server/mysql-server.js
+++ api-server/mysql-server.js
@@ -6,26 +6,6 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-    // ...
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Initialize Firebase Cloud Messaging and get a reference to the service
-const messaging = getMessaging(app);
-
-// Add the public key generated from the console here.
-messaging.getToken({vapidKey: "BHmH4iaG8JtPeSNOMTPXiPQe0GWwQGfkZnxl7-NyZuivjJFQR8FKV9hW4EuTfoQLA-gwH_CWvsA1PPS8T7kfaT0"});
-
-
 // Set CORS option
 app.use(cors());
 
